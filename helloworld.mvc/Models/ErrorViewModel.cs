e5f2ea91e180bc7e23a594d3055a287c99a5f08f
Gabriel Caragata <gabi.caragata@outlook.com>
2023-07-23 21:15:21 +0300

    wdadas

--- helloworld.mvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace helloworld.mvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
